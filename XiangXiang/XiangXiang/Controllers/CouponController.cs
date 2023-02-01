9d78b679987a28ce4725d0b740597705f0c2e5ef
LAPTOP-JBUE12H0\Acer <sam831020ya@gmail.com>
2023-02-01 23:07:45 +0800

    模板+優惠卷

--- XiangXiang/XiangXiang/Controllers/CouponController.cs
+++ XiangXiang/XiangXiang/Controllers/CouponController.cs
@@ -5,13 +5,65 @@ namespace XiangXiang.Controllers
 {
     public class CouponController : Controller
     {
-        private readonly dbXContext _conetxt;
+        private readonly dbXContext db;
         public CouponController(dbXContext conetxt)
         {
-            _conetxt = conetxt;
+            db = conetxt;
         }
-        public IActionResult Index()
+        public IActionResult List()
         {
+            IEnumerable<TCoupon> coupons = null;
+            coupons = from c in db.TCoupons select c;
+            return View(coupons);
+        }
+        public IActionResult Delete(int? id) {
+         if (id != null)                                
+            {
+                TCoupon delCoupon = db.TCoupons.FirstOrDefault(t => t.CouponId == id);
+                if(delCoupon != null)
+                db.TCoupons.Remove(delCoupon);
+                db.SaveChangesAsync();
+            }
+            return RedirectToAction("List");
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id != null)
+            {
+
+                TCoupon x = db.TCoupons.FirstOrDefault(t => t.CouponId == id);
+                if (x != null)
+                    return View(x);
+            }
+            return RedirectToAction("List");
+
+        }
+        [HttpPost]
+        public IActionResult Edit(TCouponMetadata C)
+        {
+            TCoupon x = db.TCoupons.FirstOrDefault(t => t.CouponId == C.CouponId);
+            if (x != null)
+            {
+                x.Code = C.Code;
+                x.Quantity = C.Quantity;
+                x.Discount = C.Discount;
+                x.ExpiryDate = C.ExpiryDate;
+                x.Available = C.Available;
+                db.SaveChangesAsync();
+            }
+            return RedirectToAction("List");
+
+
+        }
+        [HttpPost]
+        public IActionResult Create(TCoupon t)
+        {
+            db.TCoupons.Add(t);
+            db.SaveChangesAsync();
             return View();
         }
     }
