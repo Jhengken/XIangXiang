3b58d663003eca66627b558403ee250be86bb5d4
ChenLin <gogo335g@gmail.com>
2023-02-01 11:05:58 +0800

    no message

---
+++ XiangXiang/XiangXiang/Controllers/AOrederController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XiangXiang.Models;
+
+namespace XiangXiang.Controllers
+{
+    public class AOrderController : Controller
+    {
+        public IActionResult List()
+        {
+            IEnumerable<TAorder> aorders = null;            
+            dbXContext db = new dbXContext();
+            aorders = from t in db.TAorders select new TAorder
+                      {
+                          AorderId = t.AorderId,
+                          SupplierId = t.SupplierId,
+                          AdvertiseId = t.AdvertiseId,
+                          OrderDate = t.OrderDate,
+                          EndDate = t.EndDate,
+                          Clicks = t.Clicks,
+                          Advertise = t.Advertise,
+                          Supplier = t.Supplier,
+                      };
+            return View(aorders);
+        }
+    }
+}
